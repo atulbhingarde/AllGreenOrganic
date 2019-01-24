bf5ff3c49faaf117937c3a7534a97245ab13cbf6
ojukwuabogi <ojukwu_abogi@yahoo.com>
2019-01-23 20:15:50 -0500

    upadate seed

--- seed/allgreenorganicShema.sql
+++ seed/allgreenorganicShema.sql
@@ -1,17 +1,15 @@
 CREATE DATABASE allgreenorganic;
 
-USE allgreenorganic_db;
+USE allgreenorganic;
 
-CREATE TABLE products(
-
-item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
-farmers_name VARCHAR(100) NOT NULL
-product_name VARCHAR(100) NOT NULL,
-farmers_address VARCHAR(100) NOT NULL
-phone_number (11) INTEGER NOT NULL
-department_name VARCHAR(100) NOT NULL,
-hours_operation VARCHAR(100) NOT NULL
-price DECIMAL(11,2) NOT NULL,
-stock_quantity(11) INTEGER NOT NULL,
+CREATE TABLE contacts(
+item_id INTEGER AUTO_INCREMENT NOT NULL,
+suppliers VARCHAR(100) NOT NULL,
+farmers_address VARCHAR(100) NOT NULL,
+phone_number VARCHAR(20) NOT NULL,
+hours_operation VARCHAR(100) NOT NULL,
+rating DECIMAL(4,2),
 PRIMARY KEY(item_id)
 );
+
+
