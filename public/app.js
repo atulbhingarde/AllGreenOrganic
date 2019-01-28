0b683744eeed8dc85e40ebb2012fd810cd28e6f3
ojukwuabogi <ojukwu_abogi@yahoo.com>
2019-01-28 16:25:12 -0500

    map functionality phase1

--- public/app.js
+++ public/app.js
@@ -297,3 +297,262 @@ renderProducts();
 //   }
 //   cart= [];
 // });
+const address = [];
+const apiKey = "AIzaSyBtDXqrmP1A6NzPFxzjveh5ICYCFywHeKU";
+const placeIDs = [];
+let map;
+// let userAddress;
+let restuarantIDs = [];
+// let queryUrl;
+
+//check to see if check boxes are disabled
+let btnDisabled = false;
+
+//term to search for
+let toSearchFor = "";
+
+//checks to see if user completed form
+let isFormComplete = false;
+
+let placesToDump = [];
+
+
+
+$(document).ready(function () {
+    $('#mainTitle').textillate({ in: { effect: 'rollIn' } });
+    $(".form-check-input").on("click", function () {
+        $(this).attr("isSelected", "true");
+        $('#rest-info').empty();
+        toSearchFor = "";
+        toSearchFor = $(this).attr("data-text");
+
+        isFormComplete = true;
+        restuarantIDs = [];
+
+        if (!btnDisabled) {
+            disableBoxes(this);
+
+        } else if (btnDisabled) {
+            releaseBoxes(this);
+
+        }
+
+
+    });
+});
+//gets the address the user inputs
+const getAddress = () => {
+  let street = $("#address").val().trim();
+  let city = $("#city").val().trim();
+  let state = $("#state").val().trim();
+  let zipcode = $("#zipcode").val().trim();
+
+  //pushes it to the address array
+  address.push(street, city, state, zipcode);
+}
+console.log("javascript is hooked up")
+
+$('#submit-geo').on('click', function (event) {
+  //if the user has given an address and chosen what they want to search for
+  event.preventDefault();
+  alert("working")
+
+ console.log("in submit go")
+      getAddress();
+
+      //sets the address
+      let userAddress = address.join();
+      let coords;
+
+      let queryUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${userAddress}&key=${apiKey}`;
+      $.ajax({
+          url: queryUrl,
+          method: 'GET'
+      }).then(function (response) {
+
+        console.log(response)
+          let source = response.results;
+          for (let i = 0; i < source.length; i++) {
+              //creates a variable that will hold the users lat and lng coordinates
+              coords = { lat: source[i].geometry.location.lat, lng: source[i].geometry.location.lng };
+
+          }
+          //initialize map with user's coordinates
+          initMap(coords);
+      });
+  
+
+
+});
+
+
+//creates a map to said id
+const initMap = (coords) => {
+  map = new google.maps.Map(document.getElementById('geo-map'), {
+      center: coords,
+      //controls how 'zoomed' the map will start
+      zoom: 11
+  });
+  //recieve locations at a 10 mile radius from the user's coordinates
+  getRestPlaces(coords);
+}
+
+// returns places around the user at 10-mile radius
+const getRestPlaces = (coords) => {
+
+  let coordsArr = [];
+
+  for (num in coords) {
+      coordsArr.push(coords[num]);
+  }
+
+  let originalUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${coordsArr[0]},${coordsArr[1]}&radius=10000&keyword=${toSearchFor}&key=${apiKey}`;
+  let queryUrl = "https://cors-anywhere.herokuapp.com/" + originalUrl;
+
+  $.ajax({
+      url: queryUrl,
+      method: 'GET',
+      dataType: "json",
+      headers: {
+          "x-requested-with": "xhr"
+      }
+  }).then(function (response) {
+
+      const dynamicSort=function(property) {
+         var sortOrder = -1;
+         if(property[0] === "-") {
+            sortOrder = -1;
+            property = property.substr(1);
+          }
+          return function (a,b) {
+              var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+              return result * sortOrder;
+         }
+      }
+
+      let source = response.results;
+      for (let i = 0; i < source.length; i++) {
+          //create obj that holds an id of the place received by the api
+          let place = {
+              id: source[i].place_id,
+              rating: source[i].rating,
+          };
+          //pushes place id into restuarantIDs
+          restuarantIDs.push(place);
+          restuarantIDs.sort(dynamicSort("rating"));
+
+      }
+      //renders place markers to the map
+      renderMarks(map, restuarantIDs);
+  });
+}
+
+const renderMarks = (map, idArr) => {
+  //initialize info window
+  let infowindow = new google.maps.InfoWindow();
+  // initialize google place details service
+  let service = new google.maps.places.PlacesService(map);
+  for (let i = 0; i < 10; i++) {
+
+      service.getDetails({
+          //passes ids for each of the places in the array
+          placeId: idArr[i].id
+      }, function (place, status) {
+          
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+              //create for a place
+              let marker = new google.maps.Marker({
+                  map: map,
+                  animation: google.maps.Animation.DROP,
+                  position: place.geometry.location
+              });
+              marker.addListener('click', toggleBounce);
+              //if click on the marker, an info window will render above it showing specific detais about the place
+              google.maps.event.addListener(marker, 'click', function () {
+                  infowindow.setContent('<div id="fade-test"><strong>' + place.name + '</strong><br>' +
+                      'Rating: ' + place.rating  + checkPhone() + 
+                      getTravelUrl(address, place.formatted_address) + '<br></div>');
+                  infowindow.open(map, this);
+              });
+              function toggleBounce() {
+                  if (marker.getAnimation() !== null) {
+                      marker.setAnimation(null);
+                  } else {
+                      marker.setAnimation(google.maps.Animation.BOUNCE);
+                  }
+              }
+              //checks to see if the place has a phone
+              function checkPhone() {
+                  let phone = place.formatted_phone_number;
+
+                  if (typeof phone !== typeof undefined && phone !== false) {
+                      //if phone number is present, add phone to the content
+                      return `<br> ${phone} <br>`;
+
+                  } else {
+                      //else just had a space
+                      return `<br>`;
+                  }
+
+              }
+
+              //Function to check to retrieve opening hours for the day the applicaiton is running
+              function checkHours() {
+                  let isOpen = place.opening_hours.open_now;
+                  let hours = place.opening_hours.weekday_text;
+                  var dayOfTheWeekIndex = new Date().getDay();
+                  let displayHours = "";
+                  
+                  if(isOpen === true)
+                  {
+                      --dayOfTheWeekIndex;
+                      if (dayOfTheWeekIndex<0)
+                      {
+                          dayOfTheWeekIndex=6;
+                      }
+                      
+                      displayHours = hours[dayOfTheWeekIndex];
+                      
+                      return `${displayHours}`;
+                  }
+                  else
+                  {
+                      return `Closed`;
+
+                  }
+              }
+
+              //if the place rating is greater than or equal to 4.5, render the following content to the 'restaurant recommendations' div
+
+              if (place.rating >= 4.5) {
+
+                  $("#rest-info").append(
+                      `
+                      <div class="card my-3">
+                      <h5 class="card-header">${place.name}</h5>
+                      <div class="card-body">
+                          <!-- restaurant info get populated here -->
+                          <p class="card-text">${('<div id="fade-test">' +
+                          'Rating: ' + place.rating + '<br>Hours Open: ' + checkHours() + checkPhone() +
+                          '</div>' + getTravelUrl(address, place.formatted_address)) } </p>
+                      </div>
+                      <div class="card-footer">
+                      <!-- restaurant url population -->
+                      <a href="${place.website}" class="card-link" target="_blank">Check out menu on Website!</a>
+                      </div>
+                      </div>
+                      `
+                  );
+              }
+           }
+      });
+  }
+  
+
+}
+
+//Function to get the Travel Direction to resturant with URL 
+const getTravelUrl = (origin, destination) => {
+  return `<a href="https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}&travelmode=driving" target="_blank">${destination}</a>`;
+}
+
